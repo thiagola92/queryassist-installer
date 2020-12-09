2a54d9ef4dd96efbc8928d7d5c29bffa15935b50
thiagola92 <thiagola92@gmail.com>
2020-12-09 09:07:30 -0300

    Create script

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # QueryAssist Installer
 Download and setup QueryAssist on Ubuntu.  
+`bash install.sh`  
+
+## Appears on search
+![Find QueryAssist when searching](img/gnome-search.png)  
+
+## Initialize by Terminal
+![Initialize QueryAssist using terminal](img/gnome-terminal.png)  
---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://cdn.queryassist.com/files/QueryAssist_for_MongoDB.AppImage > queryassist;
+chmod u+x queryassist;
+sudo mv queryassist /usr/local/bin;
+
+cp queryassist.desktop /home/$USER/.local/share/applications;
+cp queryassist-icon.png /usr/share/icons;
